eba0e1967aff4e455a040c2484289721f2589eb5
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-05 18:07:19 +0000

    Added function to find all animals

--- models/animal.rb
+++ models/animal.rb
@@ -1,22 +1,30 @@
-require_relative("../db/sql_runner")
+  require_relative("../db/sql_runner")
 
-class Animal
+  class Animal
 
-  attr_accessor :id, :animal_name, :species, :breed, :admission_date, :adoption_status
+    attr_accessor :id, :animal_name, :species, :breed, :admission_date, :adoption_status
 
-  def initialize(options)
-    @id = options["id"].to_i
-  @animal_name = options["animal_name"]  
-  @species = options["species"]
-  @breed = options["breed"]
-  @admission_date = options["admission_date"]
-  @adoption_status= options["adoption_status"] 
+    def initialize(options)
+      @id = options["id"].to_i
+    @animal_name = options["animal_name"]  
+    @species = options["species"]
+    @breed = options["breed"]
+    @admission_date = options["admission_date"]
+    @adoption_status= options["adoption_status"] 
+    end
+
+    def save()
+      sql = "INSERT INTO animals (animal_name, species, breed, admission_date, adoption_status) VALUES ('#{@animal_name}','#{@species}','#{@breed}','#{@admission_date}','#{adoption_status}') RETURNING *"
+      animal = SqlRunner.run(sql)
+      @id = animal.first()['id'].to_i
+    end
+
+ def self.all()
+   sql = "SELECT * FROM animals"
+   result = SqlRunner.run(sql)
+   
+   return result.map{ |animal_hash| Animal.new(animal_hash) }
+ end
+
+
   end
-
-  def save()
-    sql = "INSERT INTO animals (animal_name, species, breed, admission_date, adoption_status) VALUES ('#{@animal_name}','#{@species}','#{@breed}','#{@admission_date}','#{adoption_status}') RETURNING *"
-    animal = SqlRunner.run(sql)
-    @id = animal.first()['id'].to_i
-  end
-
-end
