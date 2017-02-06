a9afc92ffd04f0cd1c4f34e366902d6c3cc3d36f
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-06 09:48:39 +0000

    Created views folder and animal controller file

---
+++ views/animal_controller.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+
+require_relative("../models/animal")
+
+get '/animals' do
+  @animals = Animal.all()
+  erb(:index)
+end
+
